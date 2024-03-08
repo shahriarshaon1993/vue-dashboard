4b860323db60f97a8c196a2a70667ef6a04c2dc6
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2024-03-08 12:37:57 +0600

    Pagination component imlement

---
+++ src/components/Pagination.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  links: Array
+});
+</script>
+
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="inline-flex -space-x-px text-sm">
+      <template v-for="link in links">
+        <li>
+          <router-link v-if="link.url" :to="link.url" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            {{ link.label }}
+          </router-link>
+          <span v-else class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            {{ link.label }}
+          </span>
+        </li>
+      </template>
+    </ul>
+  </nav>
+</template>
--- src/pages/elements/Paginations.vue
+++ src/pages/elements/Paginations.vue
@@ -1,3 +1,29 @@
+<script setup>
+import BreadcrumbItems from "@/components/Breadcrumb/BreadcrumbItems.vue";
+import BreadcrumbItem from "@/components/Breadcrumb/BreadcrumbItem.vue";
+import Card from "@/components/Card.vue";
+import {ref} from "vue";
+import Pagination from "@/components/Pagination.vue";
+
+// refs
+const links = ref([
+  { active: false, label: 'Previous', url: null },
+  { active: true, label: 1, url: '#' },
+  { active: false, label: 2, url: '#' },
+  { active: false, label: 3, url: '#' },
+  { active: false, label: 4, url: '#' },
+  { active: false, label: 5, url: '#' },
+  { active: false, label: 'Next', url: '#' },
+]);
+</script>
+
 <template>
-  <h2 class="text-2xl text-gray-900 dark:text-white">Pagination's</h2>
+  <BreadcrumbItems>
+    <BreadcrumbItem title="Dashboard" :href="{name: 'Dashboard'}" :home-icon="true"/>
+    <BreadcrumbItem title="Pagination" :href="{name: 'ElementPagination'}" :active="true"/>
+  </BreadcrumbItems>
+
+  <Card title="Default pagination">
+    <Pagination :links="links" />
+  </Card>
 </template>
